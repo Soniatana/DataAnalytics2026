f723dff8b248dfea45f87b9b164359efffcdd4f7
Soniatana <sonia.tana301@gmail.com>
2026-04-13 21:14:53 -0400

    Update Ex1B queries for actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,12 @@
 /*
 Sakila is a database used to collect information and used for queries
 */
+
+USE sakila;
+SELECT * FROM actor; -- Retrived [200] records
+
+SELECT * FROM film; -- Retrieved [1000] records
+
+
+
+
