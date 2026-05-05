13eaaffa9bdac813190dca56a6bd4054f03040de
Soniatana <sonia.tana301@gmail.com>
2026-05-04 23:06:25 -0400

    completed Ex1A exercise

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,17 @@
 print( 'Hello world!')
+
+message = 'Hello world!'
+print(message)
+
+# "Hello world " prints twice because it is stored in a variable called message, then i still asked it to print the varable 
+
+# Displaying dollars and cents 
+dollars = 3
+cents = 0.50
+print (dollars + cents)
+
+# The result shows 3.5 instead of 3.50 because python drops trailing zeros in decimals
+
+d_str = '3 dollars'
+c_str = '50 cents'
+print (d_str + " " + c_str )
