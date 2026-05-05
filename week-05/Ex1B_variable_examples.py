e3cb0f64283eb8deb71625b498338d764767c440
Soniatana <sonia.tana301@gmail.com>
2026-05-05 00:36:16 -0400

    Added EX1b variable

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,41 @@
+# variable examples 
+
+# 2. customer-related variables
+
+customer_id = 1001
+customer_name = "Sonia Tana"
+customer_gender = "Female"
+customer_date_of_birth = "1999-01-22"
+Drivers_license = "D1234567"
+auto_policy_number = "POL987654"
+
+# 4. personal variables
+
+my_name = "Sonia Tana" #full name stored together, but could be separated if needed
+city_state = "Charlotte, NC"
+ # combined city and state, but could be two variables for better structure 
+
+ # Full list of Python reserved words (keywords)
+ # These cannot be used as variable names:
+# False, None, True, and, as, assert, async, await,
+# break, class, continue, def, del, elif, else, except,
+# finally, for, from, global, if, import, in, is,
+# lambda, nonlocal, not, or, pass, raise, return,
+# try, while, with, yield 
+
+# 1b. Definitions of 5 keywords
+
+# def: used to define a function in Python 
+# # example: def my_function():
+
+#  # if: used to make a decision based on a condition
+# example: if x > 5:
+
+# for: used to create a loop that repeats a block of code 
+# example: for i in range(5):
+
+# return: used to send a value back from a function 
+# example: return result 
+
+# class: used to define a ^^blueprint^^ for creating objects (like a template)
+# example: class Car:
