86c88a6ca355ac2d49c01272de7a0b9d5b8eb11d
Soniatana <sonia.tana301@gmail.com>
2026-05-05 19:29:06 -0400

    area_of_rectangle.py

---
+++ week-05/area_of_rectangle.py
@@ -0,0 +1,9 @@
+# Area = length × width
+side_a =5
+side_b =15
+
+area = side_a * side_b
+
+print("Side A is " + str(side_a))
+print("Side B is " + str(side_b))
+print("The area of the rectangle is " + str(area))
