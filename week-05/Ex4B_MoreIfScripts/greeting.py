5ea4743cd3127f6d1ea9b5acbb763027b411aa43
Soniatana <sonia.tana301@gmail.com>
2026-05-08 18:55:33 -0400

    Added file to Ex4B

---
+++ week-05/Ex4B_MoreIfScripts/greeting.py
@@ -0,0 +1,10 @@
+hour = 14  # test different values like 5, 12, 20, 23
+
+if hour >= 23 or hour < 4:
+    print("What are you doing up so late??")
+elif hour < 10:
+    print("Good morning!")
+elif hour < 17:
+    print("Good day!")
+else:
+    print("Good evening!")
