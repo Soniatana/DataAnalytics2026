c90f143d978e35ff8c9c258cc27f1df4ef9d9c26
Soniatana <sonia.tana301@gmail.com>
2026-05-08 18:58:10 -0400

    Replace file with updated version

--- week-05/Ex4B_MoreIfScripts/greeting.py
+++ week-05/Ex4B_MoreIfScripts/greeting.py
@@ -8,3 +8,12 @@ elif hour < 17:
     print("Good day!")
 else:
     print("Good evening!")
+
+if hour >= 23 or hour < 4:
+    print("What are you doing up so late??")
+elif hour < 10:
+    print("Good morning!")
+elif hour < 17:
+    print("Good day!")
+else:
+    print("Good evening!")
