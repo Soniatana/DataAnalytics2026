b492a1114866246ff7764bb2d091c2c7c064b418
Soniatana <sonia.tana301@gmail.com>
2026-05-10 13:10:30 -0400

    Added file to Ex4C

---
+++ week-05/Ex4C_LoopScripts/sales_performance.py
@@ -0,0 +1,7 @@
+sales = [
+    ("Marcus Webb", "East", 4250.00),
+    ("Priya Sharma", "West", 5875.50),
+    ("DeShawn Carter", "East", 3100.75),
+    ("LaTonya Rivers", "South", 6420.00),
+    ("Bob Nguyen", "West", 4980.25)
+]
