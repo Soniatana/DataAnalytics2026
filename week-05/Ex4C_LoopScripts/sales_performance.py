2386124ffaf87bcd83955df47adf76890726e779
Soniatana <sonia.tana301@gmail.com>
2026-05-10 13:12:34 -0400

    updated file

--- week-05/Ex4C_LoopScripts/sales_performance.py
+++ week-05/Ex4C_LoopScripts/sales_performance.py
@@ -5,3 +5,15 @@ sales = [
     ("LaTonya Rivers", "South", 6420.00),
     ("Bob Nguyen", "West", 4980.25)
 ]
+
+total_sales = 0
+
+for name, region, amount in sales:
+    print(name + " (" + region + "): $" + format(amount, ".2f"))
+
+    if amount > 5000:
+        print("^ Top performer!")
+
+    total_sales += amount
+
+print("Total sales: $" + format(total_sales, ".2f"))
